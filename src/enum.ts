896ae0152905c6b5a728ed618170a84d9ba450f8
fardan <fardan.nozami@gmail.com>
2022-08-23 09:50:57 +0700

    enum

---
+++ src/enum.ts
@@ -0,0 +1,23 @@
+// enum
+
+// numeric enum
+// enum months {
+//   JAN = 1,
+//   FEB,
+//   MAR,
+//   APR,
+//   MAY,
+// }
+
+// console.log('months :', months.JAN);
+
+// string enum
+enum months {
+  JAN = 'Januari',
+  FEB = 'februari',
+  MAR = 'Maret',
+  APR = 'April',
+  MAY = 'may',
+}
+
+console.log('months :', months.JAN);
