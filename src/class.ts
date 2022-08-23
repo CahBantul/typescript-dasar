e513f4ee3109b4d31a5e564f1e7f6eb5f9d035b8
fardan <fardan.nozami@gmail.com>
2022-08-23 13:05:23 +0700

    class done

--- src/class.ts
+++ src/class.ts
@@ -3,7 +3,53 @@ export class User {
   constructor(name: string, public age: number) {
     this.name = name;
   }
+
+  setName(value: string): void {
+    this.name = value;
+  }
+
+  getName = (): string => {
+    return this.name;
+  };
 }
 
 let user = new User('Fardan', 25);
 console.log(user);
+
+// user
+// Admin
+// member
+class Admin extends User {
+  read: boolean = true;
+  write: boolean = true;
+  phone: string;
+  private _email: string = '';
+  static getRoleName: string = 'Admin';
+
+  constructor(name: string, age: number, phone: string) {
+    super(name, age);
+    this.phone = phone;
+  }
+
+  static getRoleNameMethod(): string {
+    return 'halo boss';
+  }
+
+  getRole(): { read: boolean; write: boolean } {
+    return {
+      read: this.read,
+      write: this.write,
+    };
+  }
+
+  set email(value: string) {
+    this._email = value;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+}
+
+let admin = Admin.getRoleNameMethod();
+console.log('admin', admin);
