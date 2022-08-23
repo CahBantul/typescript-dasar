4892c132cee973eb908d6f1aa1957684bfdfecf9
fardan <fardan.nozami@gmail.com>
2022-08-23 09:44:46 +0700

    basic type and array

---
+++ src/array.ts
@@ -0,0 +1,19 @@
+// array
+
+// array number
+let array: number[] = [1, 2, 3];
+array = [2, 3, 4];
+console.log('array :', array);
+
+// array string
+let array2: string[] = ['a', 'b', 'c'];
+console.log('array2 :', array2);
+
+// array any
+let array3: any[] = ['1', 2, true];
+console.log('array3 :', array3);
+
+// tuples
+let biodata: [string, number];
+biodata = ['Surabaya', 123];
+console.log('biodata :', biodata);
---
+++ src/typeDasar.ts
@@ -0,0 +1,21 @@
+// string
+let nama: string = 'fardan Nozami';
+
+nama = 'Ajitama';
+console.log('nama :', nama);
+
+// number
+let umur: number = 30;
+umur = 17;
+console.log('umur :', umur);
+
+// boolean
+let isRich: boolean = false;
+isRich = true;
+console.log('isRich :', isRich);
+
+// any
+let address: any = 'Bantul';
+address = 123;
+address = true;
+console.log('address :', address);
